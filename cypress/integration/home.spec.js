8909e46bc82994c4ba51dbefea891ee3a2a8e59e
EuphieSS <samma118@hotmail.com>
2023-03-15 03:27:20 +0000

    Update initializer and add tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('home page', () => {
+
+  it('Visit the homepage', () => {
+    cy.visit('http://localhost:3000/')
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
+});
